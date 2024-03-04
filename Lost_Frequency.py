c322f30a38bf54b61f55356df7ec12d711c28ff8
Titouan Duhaze <titouan.duhaze@telecom-paris.fr>
2024-03-04 21:11:47 +0100

    finish

--- Lost_Frequency.py
+++ Lost_Frequency.py
@@ -12,23 +12,39 @@ Alphabet = {num: chr(num - 501 + ord('A')-offset) for num in range(501+offset, 5
 
 
 def decode(file:str):
+    mot=''
     x, Fe = sf.read(file)
+    print(len(x))
     T=[i*1/Fe for i in range(len(x))]
     Tec=T[-1]
-    x_fft=np.fft.fft(x)
-    x_fft_max=np.max(np.abs(x_fft))
+    offset=0
+    while offset<=len(x)-2000:
+        x_symbole=x[offset:offset+2000]
 
-    x_fft_b=[num if num > x_fft_max/2 else 0 for num in np.abs(x_fft)]
-    show_signal_F(x_fft_b,Fe)
-    for i in range(len(x_fft_b[:len(x_fft_b)//2])):
-        if x_fft_b[i]!=0:
-            a=np.floor(i/Tec)
-            if a in Alphabet :
-                print(Alphabet[a])
-            j=0
-            while x_fft_b[i+j]!=0:
-                x_fft_b[i+j]=0
-                j+=1
+        x_fft=np.fft.fft(x_symbole)
+        #show_signal_F(np.abs(x_fft),Fe)
+        Tec=T[1999]
+        index=np.argmax(np.abs(x_fft[:len(x_fft)//2]))
+        a=np.round(index/Tec)
+        #print(a)
+        if a in Alphabet :
+                #print(Alphabet[a])
+                mot+=Alphabet[a]
+        else :
+             mot+=' '
+        offset=offset+2500
+    print(mot)
+    #x_fft_b=[num if num > x_fft_max/2 else 0 for num in np.abs(x_fft)]
+    #show_signal_F(x_fft_b,Fe)
+    #for i in range(len(x_fft_b[:len(x_fft_b)//2])):
+    #    if x_fft_b[i]!=0:
+    #        a=np.floor(i/Tec)
+    #        if a in Alphabet :
+    #            print(Alphabet[a])
+    #        j=0
+    #        while x_fft_b[i+j]!=0:
+    #            x_fft_b[i+j]=0
+    #            j+=1
     #show_signal_F(x_fft_b,Fe)
 
     
