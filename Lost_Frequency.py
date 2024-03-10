88a42c2e2bbbc5248fd01f67d8e8e6be7f191165
Titouan Duhaze <titouan.duhaze@telecom-paris.fr>
2024-03-10 20:47:26 +0100

    modif

--- Lost_Frequency.py
+++ Lost_Frequency.py
@@ -6,7 +6,7 @@ import IPython.display as ipd
 import soundfile as sf
 import sys
 
-offset=0
+offset=0    #0 ou -1
 
 Alphabet = {num: chr(num - 501 + ord('A')-offset) for num in range(501+offset, 527+offset)}
 
@@ -16,21 +16,49 @@ def decode(file:str):
     #show_signal_T(x,Fe)
     T=[i*1/Fe for i in range(len(x))]
     offset=0
+    i=0
     while offset<=len(x)-2000:
-        N=Fe*8
+        multiply=1
+        N=Fe*multiply
         x_symbole=x[offset:offset+2000]
 
         x_fft=np.fft.fft(x_symbole,N)
-        show_signal_F(np.abs(x_fft),Fe)
+        #show_signal_F(np.abs(x_fft),Fe)
         Tec=T[1999]
         index=np.argmax(np.abs(x_fft[:len(x_fft)//2]))
-        print(index/Tec*len(x_symbole)/N)
+        #print(index/Tec*len(x_symbole)/N)
         a=np.round(index/Tec*len(x_symbole)/N)
+        test=True
         if a in Alphabet :
                 mot+=Alphabet[a]
-        else :
+                test=False
+                print("basique")
+                print(Alphabet[a])
+        if a not in Alphabet and max(np.abs(x_fft[:len(x_fft)//2]))>np.mean(np.abs(x_fft[:len(x_fft)//2]))*10:
+            x_fft=np.fft.fft(x_symbole)
+            #show_signal_F(np.abs(x_fft),Fe)
+            # start=int(np.round((501+offset)*Tec))
+            # print(start)
+            # stop=int(np.round((527+offset)*Tec))
+            # print(stop)
+            x_fft[:126]=0
+            x_fft[132:]=0
+            #show_signal_F(np.abs(x_fft),Fe)
+            #index=np.argmax(np.abs(x_fft))
+            index=find_increase_then_decrease(np.abs(x_fft))
+            a=np.round(index/Tec)
+            if a in Alphabet :
+                mot+=Alphabet[a]
+                print(Alphabet[a])
+            else:
+                mot+='?'
+                print("?")
+            print("avancé")
+        elif test and max(np.abs(x_fft[:len(x_fft)//2]))<np.mean(np.abs(x_fft[:len(x_fft)//2]))*10:
              mot+=' '
+             print("espace")
         offset=offset+2500
+        i+=1
     print(mot)
     #x_fft_b=[num if num > x_fft_max/2 else 0 for num in np.abs(x_fft)]
     #show_signal_F(x_fft_b,Fe)
@@ -50,6 +78,11 @@ def decode(file:str):
     #show_signal_T(x,Fe)
     #show_signal_F(x_fft,Fe)
 
+def find_increase_then_decrease(x_fft):
+    for i in range(len(x_fft) - 2):
+        if x_fft[i] < x_fft[i+1] > x_fft[i+2]:
+            return i+1
+    return -1  # Retourne -1 si aucun point correspondant n'est trouvé
 
 
 def show_signal_T(x,Fe):
